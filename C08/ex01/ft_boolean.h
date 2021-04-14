f8957bdd2d480c48c744b031dce498b582875287
Younji Kim <obejection@kakao.com>
2021-04-14 18:31:56 +0900

    C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younjkim <younjkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 00:07:36 by younjkim          #+#    #+#             */
+/*   Updated: 2021/04/14 00:33:44 by younjkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define EVEN(nbr) (nbr % 2 == 0)
+
+typedef int		t_bool;
+
+#endif

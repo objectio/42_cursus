beafbeb9eae3a42e9cf853de8cf917f9ee7d09d1
Younji Kim <younjkim@c7r9s1.42seoul.kr>
2022-09-27 22:08:15 +0900

    [ADD] Create includes for header files and upload first header file (#2)

---
+++ ft_containers/includes/containers.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   containers.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younjkim <younjkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 21:32:40 by younjkim          #+#    #+#             */
+/*   Updated: 2022/09/27 21:33:23 by younjkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTAINERS_HPP
+# define CONTAINERS_HPP
+
+#include <iostream>
+#include <string>
+#include <deque>
+#if 1 //CREATE A REAL STL EXAMPLE
+	#include <map>
+	#include <stack>
+	#include <vector>
+	namespace ft = std;
+#else
+	#include <map.hpp>
+	#include <stack.hpp>
+	#include <vector.hpp>
+#endif
+
+#include <stdlib.h>
+
+#endif

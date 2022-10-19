8151b776a85d2a1da51ef3d3c9250b7a863b0ee2
Younji Kim <objection@kakao.com>
2022-10-19 21:16:51 +0900

    [ADD] Create vector.hpp (#2)

---
+++ ft_containers/includes/vector.hpp
@@ -0,0 +1,17 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include "containers.hpp"
+#include <memory>
+
+namespace ft 
+{
+    
+	template <class _T, class _Allocator = std::allocator<_T> >
+	class _vector {
+		
+	}
+
+}
+
+#endif

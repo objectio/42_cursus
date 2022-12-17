ae5af51f73ee712b8f62c4a35fb0893a16e1bacb
Younji Kim <objection@kakao.com>
2022-12-17 18:03:11 +0900

    [CHORE] Comment STL example out in containers.hpp

--- ft_containers/includes/containers.hpp
+++ ft_containers/includes/containers.hpp
@@ -6,7 +6,7 @@
 /*   By: younjkim <younjkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 21:32:40 by younjkim          #+#    #+#             */
-/*   Updated: 2022/12/13 16:27:26 by younjkim         ###   ########.fr       */
+/*   Updated: 2022/12/17 18:01:19 by younjkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@
 #include <iostream>
 #include <string>
 #include <deque>
-#if 1 //CREATE A REAL STL EXAMPLE
-	#include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
-#else
-	#include "map.hpp"
-	#include "stack.hpp"
-	#include "vector.hpp"
-#endif
+// #if 1 //CREATE A REAL STL EXAMPLE
+// 	#include <map>
+// 	#include <stack>
+// 	#include <vector>
+// 	namespace ft = std;
+// #else
+// 	#include "map.hpp"
+// 	#include "stack.hpp"
+// 	#include "vector.hpp"
+// #endif
+
+#include "iterator.hpp"
+#include "type_traits.hpp"
+#include "vector.hpp"
+#include "algorithm.hpp"
 
 #include <stdlib.h>
 

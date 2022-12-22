2a1c930274e8766c5fa5e7f5099a1e8ac73ad53d
Younji Kim <objection@kakao.com>
2022-12-22 21:13:20 +0900

    [FEAT] Define reverse_iterator member type (#8)

--- ft_containers/includes/iterator.hpp
+++ ft_containers/includes/iterator.hpp
@@ -52,6 +52,25 @@ namespace ft
 	typedef const T&						reference;
 	typedef random_access_iterator_tag		iterator_category;
   };
+
+  template <class Iterator> 
+  class reverse_iterator
+  {
+	  public:
+	  	typedef Iterator										iterator_type;
+		typedef iterator_traits<Iterator>::iterator_category	iterator_category;
+		typedef iterator_traits<Iterator>::value_type			value_type;
+		typedef iterator_traits<Iterator>::difference_type		difference_type;
+		typedef iterator_traits<Iterator>::pointer				pointer;
+		typedef iterator_traits<Iterator>::reference			reference;
+
+		reverse_iterator();
+		
+		explicit reverse_iterator (iterator_type it);
+		
+		template <class Iter>
+		reverse_iterator (const reverse_iterator<Iter>& rev_it);
+  };
 }
 
 #endif

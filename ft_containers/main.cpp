89ccbcfa4196cdfadf2b2ec87602df1825feed89
objectio <objection@kakao.com>
2023-01-03 23:42:14 +0900

    [TEST] ft::lexicographical_compare

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -6,7 +6,7 @@
 /*   By: younjkim <younjkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 21:30:51 by younjkim          #+#    #+#             */
-/*   Updated: 2022/12/28 20:58:46 by younjkim         ###   ########.fr       */
+/*   Updated: 2023/01/03 23:41:18 by younjkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ int main(int argc, char** argv)
 		std::cout << "Comparing foo and bar lexicographically (foo<bar):\n";
 
 		std::cout << "Using default comparison (operator<): ";
-		std::cout << std::lexicographical_compare(foo,foo+5,bar,bar+9);
+		std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9);
 		std::cout << '\n';
 
 		std::cout << "Using mycomp as comparison object: ";
-		std::cout << std::lexicographical_compare(foo,foo+5,bar,bar+9,mycomp);
+		std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9,mycomp);
 		std::cout << '\n';
 	}
 

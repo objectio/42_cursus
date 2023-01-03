c53090456cf05335af26f81eb5f3a427bc18d5d8
objectio <objection@kakao.com>
2023-01-03 23:42:53 +0900

    [FIX] Fix control reaches end of non-void function error

--- ft_containers/includes/algorithm.hpp
+++ ft_containers/includes/algorithm.hpp
@@ -10,8 +10,7 @@
 namespace ft 
 {
 	template <class InputIterator1, class InputIterator2>
-	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-	{
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
 		while (first1 != last1) {
 			if (!(*first1 == *first2))
 				return false;
@@ -22,8 +21,7 @@ namespace ft
 	}
 
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
-	{
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred) {
 		while (first1 != last1) {
 			if (!pred(*first1, *first2))
 				return false;
@@ -34,10 +32,8 @@ namespace ft
 	}
 
 	template <class InputIterator1, class InputIterator2>
-	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
-	{
-		while (first1 != last1)
-		{
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2) {
+		while (first1 != last1) {
 			if (first2 == last2 || *first2 < *first1) return false;
 			else if (*first1 < *first2) return true;
 			++first1;
@@ -48,14 +44,14 @@ namespace ft
 	}
 
 	template <class InputIterator1, class InputIterator2, class Compare>
-	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp) 
-	{
-		while (first1 != last1)
-		{
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp) {
+		while (first1 != last1) {
 			if (first2 == last2 || *first2 < *first1) return false;
 			else if (comp(*first1, *first2)) return true;
 			++first1;
 			++first2;
 		}
+
+		return (first2 != last2);
 	}
 }

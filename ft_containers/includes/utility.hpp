86a76d513824a077bdc6b5ad1dade6021357faff
objectio <objection@kakao.com>
2023-01-04 18:54:30 +0900

    [FEAT] Implement ft::make_pair() (#11)

--- ft_containers/includes/utility.hpp
+++ ft_containers/includes/utility.hpp
@@ -61,6 +61,11 @@ namespace ft {
     bool operator>= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
         return !(lhs < rhs);
     }
+
+    template <class T1, class T2>
+    pair<T1,T2> make_pair (T1 x, T2 y) {
+        return (pair<T1, T2>(x, y));
+    }
 }
 
 #endif

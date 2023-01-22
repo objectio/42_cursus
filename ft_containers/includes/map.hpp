47f62beaf5aef1efb306d855e72150b68fc3c11b
objectio <objection@kakao.com>
2023-01-22 21:19:55 +0900

    [FEAT] Implement map (#14)

--- ft_containers/includes/map.hpp
+++ ft_containers/includes/map.hpp
@@ -50,7 +50,220 @@ namespace ft {
 		typedef typename Rep_type::difference_type			difference_type;
 		typedef typename Rep_type::reverse_iterator			reverse_iterator;
 		typedef typename Rep_type::const_reverse_iterator	const_reverse_iterator;
+
+		map()
+		: m_t(Compare(), allocator_type()) { }
+
+		explicit map(const Compare& comp, const allocator_type& a = allocator_type())
+		: m_t(comp, a) { }
+
+		map(const map& x)
+		: m_t(x.m_t) { }
+
+		template <typename InputIterator>
+		map(InputIterator first, InputIterator last)
+		: m_t(Compare(), allocator_type()) {
+			m_t.insert_unique(first, last);
+		}
+
+		template <typename InputIterator>
+		map(InputIterator first, InputIterator last, const Compare& comp, const allocator_type& a = allocator_type())
+		: m_t(comp, a) {
+			m_t.insert_unique(first, last);
+		}
+
+		map& operator=(const map& x) {
+			m_t = x.m_t;
+			return (*this);
+		}
+
+		allocator_type get_allocator() const {
+			return m_t.get_allocator();
+		}
+
+		iterator begin() {
+			return (m_t.begin());
+		}
+
+		const_iterator begin() const {
+			return (m_t.begin());
+		}
+
+		iterator end() {
+			return (m_t.end());
+		}
+
+		const_iterator end() const {
+			return (m_t.end());
+		}
+
+		reverse_iterator rbegin() {
+			return (m_t.rbegin());
+		}
+
+		const_reverse_iterator rbegin() const {
+			return (m_t.rbegin());
+		}
+
+		reverse_iterator rend() {
+			return (m_t.rend());
+		}
+
+		const_reverse_iterator rend() const {
+			return (m_t.rend());
+		}
+
+		bool empty() const {
+			return (m_t.empty());
+		}
+
+		size_type size() const {
+			return (m_t.size());
+		}
+
+		size_type max_size() const {
+			return (m_t.max_size());
+		}
+
+		mapped_type& operator[](const key_type& k) {
+			iterator i = lower_bound(k);
+			if (i == end() || key_comp()(k, (*i).first))
+				i = insert(i, value_type(k, mapped_type()));
+			return ((*i).second);
+		}
+
+		mapped_type& at(const key_type& k) {
+			iterator i = lower_bound(k);
+			if (i == end() || key_comp()(k, (*i).first))
+				throw std::range_error("ft::map::at");
+			return ((*i).second);
+		}
+
+		const mapped_type& at(const key_type& k) const {
+			const_iterator i = lower_bound(k);
+			if (i == end() || key_comp()(k, (*i).first))
+				throw std::range_error("ft::map::at");
+			return ((*i).second);
+		}
+
+		ft::pair<iterator, bool> insert(const value_type& x) {
+			return (m_t.insert_unique(x));
+		}
+
+		iterator insert(iterator position, const value_type& x) {
+			return (m_t.insert_unique(position, x));
+		}
+
+		template <typename InputIterator>
+		void insert(InputIterator first, InputIterator last) {
+			m_t.insert_unique(first, last);
+		}
+
+		void erase(iterator position) {
+			m_t.erase(position);
+		}
+
+		size_type erase(const key_type& x) {
+			return (m_t.erase(x));
+		}
+
+		void erase(iterator first, iterator last) {
+			m_t.erase(first, last);
+		}
+
+		void swap(map& x) {
+			m_t.swap(x.m_t);
+		}
+
+		void clear() {
+			m_t.clear();
+		}
+
+		key_compare key_comp() const {
+			return (m_t.key_comp());
+		}
+
+		value_compare value_comp() const {
+			return (value_compare(m_t.key_comp()));
+		}
+
+		iterator find(const key_type& x) {
+			return (m_t.find(x));
+		}
+
+		const_iterator find(const key_type& x) const {
+			return (m_t.find(x));
+		}
+
+		size_type count(const key_type& x) const {
+			return (m_t.find(x) == m_t.end() ? 0 : 1);
+		}
+
+		iterator lower_bound(const key_type& x) {
+			return (m_t.lower_bound(x));
+		}
+
+		const_iterator lower_bound(const key_type& x) const {
+			return (m_t.lower_bound(x));
+		}
+
+		iterator upper_bound(const key_type& x) {
+			return (m_t.upper_bound(x));
+		}
+
+		const_iterator upper_bound(const key_type& x) const {
+			return (m_t.upper_bound(x));
+		}
+
+		ft::pair<iterator, iterator> equal_range(const key_type& x) {
+			return (m_t.equal_range(x));
+		}
+
+		ft::pair<const_iterator, const_iterator> equal_range(const key_type& x) const {
+			return (m_t.equal_range(x));
+		}
+
+		template <typename _Key, typename _T, typename _Compare, typename _Allocator>
+		friend bool operator==(const map<_Key, _T, _Compare, _Allocator>&, const map<_Key, _T, _Compare, _Allocator>&);
+
+		template <typename _Key, typename _T, typename _Compare, typename _Allocator>
+		friend bool operator<(const map<_Key, _T, _Compare, _Allocator>&, const map<_Key, _T, _Compare, _Allocator>&);
 	};
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator==(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return (x.m_t == y.m_t);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator<(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return (x.m_t < y.m_t);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator!=(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return !(x == y);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator>(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return (x > y);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator<=(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return !(x > y);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	bool operator>=(const map<Key, T, Compare, Allocator>& x, const map<Key, T, Compare, Allocator>& y) {
+		return !(x < y);
+	}
+
+	template <typename Key, typename T, typename Compare, typename Allocator>
+	void swap(map<Key, T, Compare, Allocator>& x, map<Key, T, Compare, Allocator>& y) {
+		x.swap(y);
+	}
 }
 
 #endif

870e9a4940b7ef9f41f878f1aea7e14e1ac93fe1
Younji Kim <objection@kakao.com>
2023-01-23 18:41:33 +0900

    [ADD] Make set.hpp (#2)

---
+++ ft_containers/includes/set.hpp
@@ -0,0 +1,14 @@
+#ifndef SET_HPP
+#define SET_HPP
+
+#include "containers.hpp"
+#include "map.hpp"
+
+namespace ft {
+    template < class T, class Compare = std::less<T>, class Alloc = std::allocator<T> >
+    class set {
+        
+    };
+}
+
+#endif

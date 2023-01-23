b1be811e53483f607d77cc332e7a0f1d1e40c1a2
Younji Kim <objection@kakao.com>
2023-01-23 18:41:08 +0900

    [FEAT] Implement stack complete (#18)

--- ft_containers/includes/stack.hpp
+++ ft_containers/includes/stack.hpp
@@ -41,7 +41,49 @@ namespace ft {
 		value_type& top() {
 			return (c.back());
 		}
+
+		const value_type& top() const {
+			return (c.back());
+		}
+
+		void push (const value_type& val) {
+			c.push_back(val);
+		}
+
+		void pop() {
+			c.pop_back();
+		}
 	};
+
+	template <class T, class Container>
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return (lhs.c == rhs.c);
+	}
+
+	template <class T, class Container>
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return (lhs.c < rhs.c);
+	}
+
+	template <class T, class Container>
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs == rhs);
+	}
+
+	template <class T, class Container>
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(rhs < lhs);
+	}
+
+	template <class T, class Container>
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return (rhs < lhs);
+	}
+
+	template <class T, class Container>
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs < rhs);
+	}
 }
 
 #endif

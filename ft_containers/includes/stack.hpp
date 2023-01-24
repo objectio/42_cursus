e2585a7ef7260394f25b13f5fb512af6afeecd35
Younji Kim <objection@kakao.com>
2023-01-25 01:16:20 +0900

    [CHORE] Add commentary in stack.hpp

--- ft_containers/includes/stack.hpp
+++ ft_containers/includes/stack.hpp
@@ -37,29 +37,53 @@ namespace ft {
 			return (c.size());
 		}
 
-		//
+		// Returns a read/write reference to the data at the first element of the %stack.
 		value_type& top() {
 			return (c.back());
 		}
 
+		// Returns a read-only (constant) reference to the data at the first element of the %stack.
 		const value_type& top() const {
 			return (c.back());
 		}
 
+		/*
+		 * @brief	Add data to the top of the %stack.
+		 * @param	val	Data to be added.
+		 * 
+		 * This is a typical %stack operation.
+		 * The function creates an element at the top of the %stack 
+		 * and assigns the given data to it.
+		 */
 		void push (const value_type& val) {
 			c.push_back(val);
 		}
 
+		/*
+		 * @brief	Removes first element.
+		 */
 		void pop() {
 			c.pop_back();
 		}
 	};
 
+	/*
+	 * @brief	Stack equality comparison.
+	 * @param	lhs	A %stack.
+	 * @param	rhs	A %stack of the same type as @a x.
+	 * @return	True if the size and elements of the stacks are equal.
+	 */
 	template <class T, class Container>
 	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
 		return (lhs.c == rhs.c);
 	}
 
+	/*
+	 * @brief	Stack ordering relation.
+	 * @param	lhs	A %stack.
+	 * @param	rhs	A %stack of the same type as @a lhs.
+	 * @return	True if @a lhs is lexicographically less than @a rhs.
+	 */
 	template <class T, class Container>
 	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
 		return (lhs.c < rhs.c);

37761a17d34a7f826a18cc7cb49afb91593ee328
objectio <objection@kakao.com>
2023-04-14 01:38:51 +0900

    printf.c code upload

---
+++ exam/exam02/printf.c
@@ -0,0 +1,87 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	len;
+
+	len = 0;
+	if (!str)
+		str = "(null)";
+	while (*str)
+		len += write(1, str++, 1);
+	return (len);
+}
+
+int	ft_putnbr(int nb)
+{
+	long long	n;
+	int			len;
+
+	len = 0;
+	n = nb;
+	if (n < 0)
+	{
+		len += write(1, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		len += ft_putnbr(n / 10);
+		len += ft_putnbr(n % 10);
+	}
+	else
+		len += ft_putchar(n + 48);
+	return (len);
+}
+
+int	ft_puthex(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	if (n > 15)
+	{
+		len += ft_puthex(n / 16);
+		len += ft_puthex(n % 16);
+	}
+	else if (n > 9)
+		len += ft_putchar(n + 87);
+	else
+		len += ft_putchar(n + 48);
+	return (len);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int			i;
+	int			len;
+	va_list		ptr;
+
+	len = 0;
+	i = -1;
+	va_start(ptr, format);
+	while (format[++i])
+	{
+		if (format[i] != '%')
+			len += write(1, &format[i], 1);
+		else if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if (format[i] == 's')
+				len += ft_putstr(va_arg(ptr, char *));
+			else if (format[i] == 'x')
+				len += ft_puthex(va_arg(ptr, unsigned int));
+			else if (format[i] == 'd')
+				len += ft_putnbr(va_arg(ptr, int));
+		}
+	}
+	va_end(ptr);
+	return (len);
+}
